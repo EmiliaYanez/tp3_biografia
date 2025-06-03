2b92df1fbed8e965ccc6e4a19806f80244dc997c
EmiliaYañez <yanezmariaemilia3@gmail.com>
2025-06-02 21:51:51 -0300

    subiendo biografia

---
+++ ejemplo.php
@@ -0,0 +1,34 @@
+<html>
+    ‹head›
+        <style>
+        ‹p›{
+            color:red;}
+         *{
+            color: yellow; } 
+        </style>
+    </head>
+        <head>
+            <meta charset="UTF-8/>
+            <meta lang="es-ar"/>
+                <title>5º 2º</title>
+        </head>
+            <body>
+                <header>
+                    <h2> Todos los de 5º 2 º</h2>
+                </header>
+  <main>
+     </section>
+          <h2> Alumnos</h2>
+    </section>
+        <section>
+        <h2>Alumnos que se portan mal</h2>
+            <ul>
+                <li>morena</li>
+                <li>malena</li>
+                <li>jazmin</li>
+            </ul>
+        </section>
+        </footer>
+            </body>
+   </html>
+ 
